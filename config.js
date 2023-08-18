2a994778b1e41f0a1cd7397791f83e61b7ff319e
Ali Özkara <a.oezkara@metacarp.de>
2023-08-18 12:53:22 +0200

    firstCommit

---
+++ config.js
@@ -0,0 +1,110 @@
+// const sql = require("mssql/msnodesqlv8");
+ const { Connection } = require("tedious");
+
+// const config =  sql.connect( {server: "edno.database.windows.net",
+// database: "EdnoSchoolManagementSystem",
+// driver: "msnodesqlv8",
+// user:"systemadmin",
+// password:"Klmn-32553255",
+// options: {
+//     trustedConnection: true
+// }
+// })
+
+
+
+
+// const sql = require("mssql/msnodesqlv8");
+
+// async function connectToDatabase() {
+//   try {
+//     const pool = await sql.connect({
+//       server: "edno.database.windows.net",
+//       database: "EdnoSchoolManagementSystem",
+//       user: "systemadmin", 
+//       password: "Klmn-32553255", 
+//       options: {
+//         encrypt: true,
+//       },
+//     });
+
+    
+//     console.log("Connected to the database successfully!");
+
+
+//   } catch (err) {
+//     console.error("Error connecting to the database:", err.message);
+//   }
+// }
+
+// // Call the function to connect to the database
+// connectToDatabase();
+
+
+
+
+// const sql = require("mssql/msnodesqlv8");
+
+// async function connectToDatabase() {
+//   try {
+//     const pool = await sql.connect({
+//       server: "coursemanagementsystem.cfgtwzvplpnc.eu-north-1.rds.amazonaws.com",
+//       port: 1433,
+//       database: "EdnoSchoolManagementSystem",
+//       user: "admin", 
+//       password: "Klmn-32553255", 
+//       options: {
+//         // encrypt: true,
+//         // trustServerCertificate: true,
+//       },
+//     });
+
+    
+//     console.log("Connected to the database successfully!");
+
+
+//   } catch (err) {
+//     console.error("Error connecting to the database:", err.message);
+//   }
+// }
+
+// // Call the function to connect to the database
+// connectToDatabase();
+
+
+
+
+
+
+
+
+
+
+const sql = require('mssql');
+
+async function connectToDatabase() {
+  try {
+    const config = {
+      user: 'admin',
+      password: 'Klmn-32553255',
+      server: 'coursemanagementsystem.cfgtwzvplpnc.eu-north-1.rds.amazonaws.com',
+      database: 'EdnoSchoolManagementSystem',
+      options: {
+         encrypt: true,
+      trustServerCertificate: true,
+      }
+    };
+
+    await sql.connect(config);
+
+    console.log('Connected to the database successfully!');
+
+    // Bağlantıyı kapatmak için gerekirse kullanabilirsiniz
+    // sql.close();
+  } catch (err) {
+    console.error('Error connecting to the database:', err.message);
+  }
+}
+
+// Fonksiyonu çağırarak veritabanına bağlanın
+connectToDatabase();
